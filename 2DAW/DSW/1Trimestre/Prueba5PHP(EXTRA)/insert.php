af04565673b80c12ab14d207a3aa1a3a5e84209c
Sylvan0r <adfor409@gmail.com>
2025-09-23 16:36:30 +0100

    Cambios hechos en clase

--- 2DAW/DSW/1Trimestre/Prueba5PHP(EXTRA)/insert.php
+++ 2DAW/DSW/1Trimestre/Prueba5PHP(EXTRA)/insert.php
@@ -13,27 +13,30 @@
     }
 
     // prepare and bind
-    $stmtcr = $conn->prepare("CREATE TABLE IF NOT EXISTS MyGuests (firstname varchar(255), lastname varchar(255), email varchar(255))");
+    $stmtcr = $conn->prepare("CREATE TABLE IF NOT EXISTS MyGuests2 (firstname varchar(255), lastname varchar(255), email varchar(255), passwd varchar(255))");
     $stmtcr->execute();
     $stmtcr->close();
 
-    $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $firstname, $lastname, $email);
+    $stmt = $conn->prepare("INSERT INTO MyGuests2 (firstname, lastname, email, passwd) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $firstname, $lastname, $email, $passwd);
 
     // set parameters and execute
     $firstname = "John";
     $lastname = "Doe";
     $email = "john@example.com";
+    $passwd = "1234";
     $stmt->execute();
 
     $firstname = "Mary";
     $lastname = "Moe";
     $email = "mary@example.com";
+    $passwd = "5678";
     $stmt->execute();
 
     $firstname = "Julie";
     $lastname = "Dooley";
     $email = "julie@example.com";
+    $passwd = "9101";
     $stmt->execute();
 
     echo "New records created successfully";
