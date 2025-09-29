a0af3ed56baa3f2a5d583555f1499f299b54bb5d
Sylvan0r <adfor409@gmail.com>
2025-09-29 18:39:46 +0100

    Cambios en ultimas tareas de DEW y DSW

--- 2DAW/DEW/1Trimestre/Unidad2/Actividad2/Ejercicio2/script.js
+++ 2DAW/DEW/1Trimestre/Unidad2/Actividad2/Ejercicio2/script.js
@@ -6,24 +6,33 @@ if(but){
 function bt(){
     let value1 = document.getElementById("age").value;
     var gender = document.getElementById("gender").value;
-    
-    console.log(value1);
-    console.log(gender);
-}
+    let result = document.getElementById("resultado");
+    let calcH = 220-value1;
+    let calcM = 226-value1;
 
-function genderVal(value){
-    switch(true){
-        case selected="masc":
-            console.log("ninio");
+    switch(gender){
+        case "masc":
+            result.innerHTML = "Para un hombre de "+value1+" años la FCM seria de "+calcH;
+            extra(calcH);
             break;
-        case selected="fem":
-            console.log("ninia");
-            break;
-        case selected="other":
+        case "fem":
+            result.innerHTML = "Para una mujer de "+value1+" años la FCM seria de "+calcM;            
+            extra(calcM);    
+            break;                      
+        default:
             console.log("que");
             break;
-        default:
-            console.log("ERROR");
-            break;        
     }
+}
+
+function extra(value){
+    let zonaRec = document.getElementById("zonaRec");
+    let zonaAer = document.getElementById("zonaAer");
+    let zonaAnaer = document.getElementById("zonaAnaer");
+    let lineaRoja = document.getElementById("lineaRoja");
+    
+    zonaRec.innerHTML = "Zona de recuperación: "+value*0.6 +" - "+value*0.7;
+    zonaAer.innerHTML = "Zona de recuperación: "+value*0.7 +" - "+value*0.8;
+    zonaAnaer.innerHTML = "Zona de recuperación: "+value*0.8 +" - "+value*0.9;
+    lineaRoja.innerHTML = "Zona de recuperación: "+value*0.9 +" - "+value*1;
 }
